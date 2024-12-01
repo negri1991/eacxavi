3a6978ee528d6db938151a2e4ef717d9ba5be750
Xavi <el_pecas91@hotmail.com>
2024-12-01 17:58:31 +0100

    Versio 1.0.1 Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContacteComponent } from './contacte/contacte.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'contacte', component: ContacteComponent },
+  { path: '', redirectTo: '/contacte', pathMatch: 'full' } // Ruta arrel redireccionarà a /contacte
+];
